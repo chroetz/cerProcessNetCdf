22c8b9fd9416ad14a500ae97739d46c53d6d9e9d
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-01-17 11:14:04 +0100

    moved util

--- R/ProcessNetCdf-package.R
+++ R/ProcessNetCdf-package.R
@@ -5,7 +5,7 @@
 #' @importFrom tibble tibble as_tibble
 #' @importFrom rlang .env .data
 #' @importFrom RNetCDF open.nc var.get.nc var.inq.nc close.nc create.nc var.def.nc dim.def.nc file.inq.nc var.put.nc read.nc att.get.nc att.inq.nc dim.inq.nc att.put.nc
-#' @importFrom EbmUtility setupBatches
+#' @importFrom EbmUtility setupBatches hasValue hasValueString removeFileNameEnding
 NULL
 
 ## usethis namespace: start
